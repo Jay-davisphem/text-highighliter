cd8d7154b92d7468bb6eae4bdb3f1562693d9a1d
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-06-11 02:23:54 +0100

    change letter x to x icon

--- src/components/AnnotationItem.jsx
+++ src/components/AnnotationItem.jsx
@@ -1,10 +1,11 @@
 import Modal from "./Modal";
 import useModal from "./hooks/useModal";
+import x from './x.svg'
 
 function AnnotationItem({name, type}) {
   const {isvisible, toggleModal} = useModal();
-  return <><li><span></span>{name}<span>{type}</span><span onClick={toggleModal}>x</span>
-</li>;
+  return <><li><span></span>{name}<span>{type}</span><img src={x} onClick={toggleModal} />
+</li>
     <Modal isVisible={isvisible} hideModal={toggleModal} />
   </>
 }
